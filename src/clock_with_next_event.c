fc8e269eef8c0e2e2e8107b34a1cdfc55d742fa6
Burgy Benjamin <minidfx@gmail.com>
2016-02-16 19:06:02 +0100

    First version with a line drawing between date and time.

--- src/clock_with_next_event.c
+++ src/clock_with_next_event.c
@@ -1,8 +1,15 @@
 #include <pebble.h>
 
 static Window *window;
-static TextLayer *time_layer;
-static TextLayer *date_layer;
+static TextLayer *time_layer_ptr;
+static TextLayer *date_layer_ptr;
+static GPath *line_path_ptr;
+static Layer *path_layer;
+
+static const GPathInfo LINE_PATH_INFO = {
+  .num_points = 2,
+  .points = (GPoint []) {{10, 101}, {130, 101}}
+};
 
 static void update_time(struct tm *tick_time) {
   static char s_time_buffer[8];
@@ -11,40 +18,52 @@ static void update_time(struct tm *tick_time) {
   strftime(s_time_buffer, sizeof(s_time_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
   strftime(s_date_buffer, sizeof(s_date_buffer), "%B %d", tick_time);
 
-  text_layer_set_text(time_layer, s_time_buffer);
-  text_layer_set_text(date_layer, s_date_buffer);
+  text_layer_set_text(time_layer_ptr, s_time_buffer);
+  text_layer_set_text(date_layer_ptr, s_date_buffer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time(tick_time);
 }
 
+static void draw_line_callback(Layer *layer, GContext *context) {
+  line_path_ptr = gpath_create(&LINE_PATH_INFO);
+
+  // Fill the path:
+  graphics_context_set_fill_color(context, GColorWhite);
+  gpath_draw_filled(context, line_path_ptr);
+
+  // Stroke the path:
+  graphics_context_set_stroke_color(context, GColorBlack);
+  gpath_draw_outline(context, line_path_ptr);
+}
+
 static void display_clock(Layer *window_layer, GRect bounds) {
   // Create the TextLayer with specific bounds
-  time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(60, 95), bounds.size.w, 50));
+  time_layer_ptr = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(60, 95), bounds.size.w, 42));
 
   // Improve the layout to be more like a watchface
-  text_layer_set_background_color(time_layer, GColorClear);
-  text_layer_set_text_color(time_layer, GColorBlack);
-  text_layer_set_font(time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
+  text_layer_set_background_color(time_layer_ptr, GColorClear);
+  text_layer_set_text_color(time_layer_ptr, GColorBlack);
+  text_layer_set_font(time_layer_ptr, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(time_layer_ptr, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer, text_layer_get_layer(time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(time_layer_ptr));
 }
 
 static void display_date(Layer *window_layer, GRect bounds) {
   // Create the TextLayer with specific bounds
-  date_layer = text_layer_create(GRect(18, PBL_IF_ROUND_ELSE(60, 70), bounds.size.w, 50));
+  date_layer_ptr = text_layer_create(GRect(18, PBL_IF_ROUND_ELSE(60, 70), bounds.size.w, 30));
 
   // Improve the layout to be more like a watchface
-  text_layer_set_background_color(date_layer, GColorClear);
-  text_layer_set_text_color(date_layer, GColorBlack);
-  text_layer_set_font(date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-  text_layer_set_text_alignment(date_layer, GTextAlignmentLeft);
+  text_layer_set_background_color(date_layer_ptr, GColorClear);
+  text_layer_set_text_color(date_layer_ptr, GColorBlack);
+  text_layer_set_font(date_layer_ptr, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(date_layer_ptr, GTextAlignmentLeft);
 
   // Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer, text_layer_get_layer(date_layer));
+  layer_add_child(window_layer, text_layer_get_layer(date_layer_ptr));
 }
 
 static void window_load(Window *window) {
@@ -54,6 +73,10 @@ static void window_load(Window *window) {
 
   display_clock(window_layer, bounds);
   display_date(window_layer, bounds);
+
+  path_layer = layer_create(bounds);
+  layer_set_update_proc(path_layer, draw_line_callback);
+  layer_add_child(window_layer, path_layer);
 
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
@@ -66,7 +89,10 @@ static void window_load(Window *window) {
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(time_layer);
+  text_layer_destroy(time_layer_ptr);
+  text_layer_destroy(date_layer_ptr);
+
+  gpath_destroy(line_path_ptr);
 }
 
 static void init(void) {
